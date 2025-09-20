3cb399f7e1726c6d553b712e5f4ace5024638db7
konstantin.rashevskiy <konstantin.r@danko.team>
2025-09-21 01:04:42 +0700

    доработка камеры

--- Assets/MGS-FF/SurveillanceCamera/RotationState.cs
+++ Assets/MGS-FF/SurveillanceCamera/RotationState.cs
@@ -1,8 +1,10 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 
 public class RotationState : MonoBehaviour
 {
+    [SerializeField] private float _time = 4;
     private Vector3 _localRotation;
     private Vector3 _left;
     private Vector3 _right;
@@ -13,11 +15,36 @@ public class RotationState : MonoBehaviour
         _localRotation = transform.localRotation.eulerAngles;
         _left = new Vector3(_localRotation.x, -65, _localRotation.z);
         _right = new Vector3(_localRotation.x, 65, _localRotation.z);
+        
+        
     }
 
     private void OnEnable()
     {
-        transform.DOLocalRotate(_left, 2).SetEase(Ease.Linear).OnComplete(StartLoop).SetId(0);
+        var value = Calculate(NormalizeAngle(transform.localEulerAngles.y));
+        transform.DOLocalRotate(_left, _time * value).SetEase(Ease.Linear).OnComplete(StartLoop).SetId(0);
+    }
+    
+    private float NormalizeAngle(float angle)
+    {
+        while (angle > 180f) angle -= 360f;
+        while (angle < -180f) angle += 360f;
+        return angle;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.LogError(Calculate(NormalizeAngle(transform.localEulerAngles.y)));
+        }
+    }
+
+    private float Calculate(float angle)
+    {
+        float all = 65 * 2;
+        angle += 65;
+        return angle / all;
     }
 
     private void StartLoop()
@@ -25,10 +52,10 @@ public class RotationState : MonoBehaviour
         var sequence = DOTween.Sequence();
         sequence.SetId(1);
         sequence.AppendInterval(2);
-        sequence.Append(transform.DOLocalRotate(_right, 4)).SetEase(Ease.Linear);
+        sequence.Append(transform.DOLocalRotate(_right, _time)).SetEase(Ease.Linear);
         sequence.AppendInterval(2);
         
-        sequence.Append(transform.DOLocalRotate(_left, 4)).SetEase(Ease.Linear);
+        sequence.Append(transform.DOLocalRotate(_left, _time)).SetEase(Ease.Linear);
         sequence.SetLoops(-1);
     }
 
